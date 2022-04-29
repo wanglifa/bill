2747bbdae579150ecee4be1817e36330567cc962
wanglifa <wanglifa1995@gmail.com>
2022-04-29 11:29:57 +0800

    feat: 创建 validation controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  get 'validation_codes/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   namespace :api do
-    namespace :vi do
+    namespace :v1 do
       # /api/v1
       resources :validation_codes, only: [:create]
       resource :session, only: [:create, :destroy]
