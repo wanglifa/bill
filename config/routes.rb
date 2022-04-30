91ce930843fccd89bfcfc2b6ec3284716d420a39
wanglifa <wanglifa1995@gmail.com>
2022-04-30 20:10:24 +0800

    feat: 创建根路由

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'validation_codes/create'
+  get '/', to: 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   namespace :api do
