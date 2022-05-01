50643029d1470f538665e0b42c120ae58a624d03
wanglifa <wanglifa1995@gmail.com>
2022-05-01 19:12:33 +0800

    feat: 创建 production key

--- bin/setup_host.sh
+++ bin/setup_host.sh
@@ -6,7 +6,7 @@ version=$(cat mangosteen_deploy/version)
 echo 'docker build ...'
 docker build mangosteen_deploy -t mangosteen:$version
 echo 'docker run ...'
-docker run -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
+docker run -e RAILS_MASTER_KEY=$RAILS_MASTER_KEY -d -p 3000:3000 --network=network1 -e DB_PASSWORD=$DB_PASSWORD --name=$container_name mangosteen:$version
 echo 'docker exec ...'
 docker exec -it $container_name bin/rails db:create db:migrate
 echo 'DONE!'
