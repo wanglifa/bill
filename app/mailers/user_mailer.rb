54edfc62c39405b93e94634e4ff11add1e4dee42
wanglifa <wanglifa1995@gmail.com>
2022-06-19 14:39:33 +0800

    feature: 发邮件时找最新发送的 code

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome_email(email)
-    validation_code = ValidationCode.find_by_email(email)
+    # 通过 created_at 创建时间排序 :desc 最大的排在最前面
+    validation_code = ValidationCode.order(created_at: :desc).find_by_email(email)
     @code = validation_code.code
     mail(to: email, subject: '山竹记账验证码')
   end
