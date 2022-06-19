534f57ebcb2c40559ef4986c0735809a01eb666a
wanglifa <wanglifa1995@gmail.com>
2022-06-19 13:27:03 +0800

    test: 测试邮件被成功发送

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,6 +1,7 @@
 class UserMailer < ApplicationMailer
   def welcome_email(code)
-    @code = code
-    mail(to: "wanglifa1995@qq.com", subject: '周杰伦')
+    validation_code = ValidationCode.find_by_email(email)
+    @code = validation_code.code
+    mail(to: email, subject: '山竹记账验证码')
   end
 end
