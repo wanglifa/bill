a84763745ee57637fde76453d89ec5a8dedb0aa1
wanglifa <wanglifa1995@gmail.com>
2022-06-19 14:35:45 +0800

    refactor: validationCode

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  def welcome_email(code)
+  def welcome_email(email)
     validation_code = ValidationCode.find_by_email(email)
     @code = validation_code.code
     mail(to: email, subject: '山竹记账验证码')
