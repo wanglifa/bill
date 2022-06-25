7c63e291450d2c1539b04bb172d46b9037a75e64
wanglifa <wanglifa1995@gmail.com>
2022-06-25 18:28:29 +0800

    feat: 实现 /api/v1/items 筛选功能

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,8 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    items = Item.page(params[:page])
+    # 根据 created_at 字段筛选，起始时间到结束时间 params[:created_after]..params[:created_before]
+    items = Item.where({created_at: params[:created_after]..params[:created_before]})
+      .page(params[:page])
     render json: { resources: items, pager: {
       page: params[:page],
       per_page: 10,
