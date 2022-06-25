870a6d7853b37d0ad5ff3f821a68346a34fa6391
wanglifa <wanglifa1995@gmail.com>
2022-06-25 21:01:03 +0800

    feat: 给 user 添加 jwt 相关的两个方法

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,11 +1,13 @@
 class Api::V1::ItemsController < ApplicationController
   def index
+    current_user_id = request.env['current_user_id']
+    return head :unauthorized if current_user_id.nil?
     # 根据 created_at 字段筛选，起始时间到结束时间 params[:created_after]..params[:created_before]
-    items = Item.where({created_at: params[:created_after]..params[:created_before]})
+    items = Item.where({user_id: current_user_id}).where({created_at: params[:created_after]..params[:created_before]})
       .page(params[:page])
     render json: { resources: items, pager: {
-      page: params[:page],
-      per_page: 10,
+      page: params[:page] || 1,
+      per_page: Item.default_per_page,
       count: Item.count
     } }
   end
