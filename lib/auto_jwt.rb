798e6d7a1a9e6579cac4aa834236e425fce6ef7d
wanglifa <wanglifa1995@gmail.com>
2022-06-25 21:51:28 +0800

    feat: 设置 jwt 过期

--- lib/auto_jwt.rb
+++ lib/auto_jwt.rb
@@ -3,16 +3,20 @@ class AutoJwt
     @app = app
   end
   def call(env)
-    # 获取 header 里的 authorization
-    header = env['HTTP_AUTHORIZATION'] 
-    # 从authorization获取 jwt
+    # jwt 跳过以下路径
+    return @app.call(env) if ['/api/v1/session'].include? env['PATH_INFO']
+
+    header = env['HTTP_AUTHORIZATION']
     jwt = header.split(' ')[1] rescue ''
-    # 获取解密后的结果 
-    payload = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: 'HS256' } rescue nil
-    # 拿到 payload 第一部分里的user_id把它放到 env['current_user_id'] 里
+    begin
+      payload = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: 'HS256' } 
+    rescue JWT::ExpiredSignature
+      return [401, {}, [JSON.generate({reason: 'token已过期'})]]
+    rescue  
+      return [401, {}, [JSON.generate({reason: 'token无效'})]]
+    end
     env['current_user_id'] = payload[0]['user_id'] rescue nil
-    # 执行我们所有的 controller，@status, @headers, @response 这三个变量是执行每个 controller 给我们返回的
     @status, @headers, @response = @app.call(env)
     [@status, @headers, @response]
   end
-end 
+end
