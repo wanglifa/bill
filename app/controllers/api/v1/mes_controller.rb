11ed37d37d5b2ce915259f12ffdd8004f6d5c0db
wanglifa <wanglifa1995@gmail.com>
2022-07-02 15:48:31 +0800

    feat: 测试

--- app/controllers/api/v1/mes_controller.rb
+++ app/controllers/api/v1/mes_controller.rb
@@ -1,18 +1,8 @@
 class Api::V1::MesController < ApplicationController
   def show
-    header = request.headers['Authorization']
-    # 如果 header.split(' ')[1] 获取报错就让它等于空字符串，rescue 就相当于 try catch
-    jwt = header.split(' ')[1] rescue ''
-    payload = JWT.decode jwt, Rails.application.credentials.hmac_secret, true, { algorithm: 'HS256' } rescue ''
-    if payload.nil?
-      return head 400
-    end
-    user_id = payload[0]['user_id'] rescue nil
+    user_id = request.env['current_user_id'] rescue nil
     user = User.find user_id
-    if user.nil?
-      head 404
-    else
-      render json: { resource: user }
-    end
-  end 
+    return head 404 if user.nil?
+    render json: { resource: user }
+  end
 end
--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -34,10 +34,9 @@ class Api::V1::TagsController < ApplicationController
       render json: {errors: tag.errors}, status: :unprocessable_entity
     end
   end
-
   def destroy
     tag = Tag.find params[:id]
-    return head :forbidden unless tag.user_id === request.env['current_user_id']
+    return head :forbidden unless tag.user_id == request.env['current_user_id']
     tag.deleted_at = Time.now
     if tag.save
       head 200
